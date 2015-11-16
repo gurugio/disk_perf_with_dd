ca11c5936eb98dc13bf8a59a33e60fe8d1ffa99b
Gioh Kim <gi-oh.kim@profitbricks.com>
2015-11-16 18:33:32 +0100

    Read test must not touch device file directly
    - don't touch device file directly, it's too dangerous.
      - add drop-cache before read test
    - take short time not to effect system performance.
      - decrease bs and count values

--- latency_per_io.py
+++ latency_per_io.py
@@ -3,26 +3,26 @@ import os
 import sys
 from subprocess import call
 
-DEBUG = 0
-DATA_SIZE = 1*1024*1024*1024 #1G
+DEBUG = 1
 
 # rw: 1-READ, 2-WRITE
 # bs: string value of bs parameter
 #
-# read stat file of disk and extract ratio of sector per milli-second.
-# We can generate various sizes IO with dd and bs option
-# and get sector per ms for each IO size.
-def get_factor(file, rw, dd_bs):
-    dd_count = str(DATA_SIZE / dd_bs)
-    
+# read stat file of disk and extract ratio of sector per milli-second
+# of various IO size
+def get_factor(file, device, rw, dd_bs, dd_count):
     if (rw == 1): # READ
-        parm_if = "/dev/sdb"
+        # READ test needs actual device to generate physical IO.
+        # File reading can happen on buffered file in memory.
+        # parm_if = "/dev/" + device
+        parm_if = "./big"
         parm_of = "/dev/null"
         parm_flag = "iflag=direct,dsync"
         io_count_idx = 0
         sector_idx = 2
         tick_idx = 3
-        command = "dd"+" if="+parm_if+" of="+parm_of+" "+parm_flag+" bs="+str(dd_bs)+" count="+dd_count
+        # drop buffer/cache to read disk data
+        command = "echo 3 | sudo tee /proc/sys/vm/drop_caches" + "dd"+" if="+parm_if+" of="+parm_of+" "+parm_flag+" bs="+str(dd_bs)+" count="+str(dd_count)
     else: # WRITE
         parm_if = "/dev/zero"
         parm_of = "./big"
@@ -30,8 +30,7 @@ def get_factor(file, rw, dd_bs):
         io_count_idx = 4
         sector_idx = 6
         tick_idx = 7
-        command = "dd"+" if="+parm_if+" of="+parm_of+" "+parm_flag+" bs="+str(dd_bs)+" count="+dd_count
-
+        command = "dd"+" if="+parm_if+" of="+parm_of+" "+parm_flag+" bs="+str(dd_bs)+" count="+str(dd_count)
 
     file.seek(0)
     line = file.read()
@@ -58,20 +57,19 @@ def get_factor(file, rw, dd_bs):
         print 'sector-per-io={0}'.format((sector_after - sector_before) / (io_count_after - io_count_before))
         print 'sector-per-ms={0}'.format((sector_after - sector_before) / (ticks_after - ticks_before))
 
-    #assert sector_after != sector_before
-    #assert ticks_after != ticks_before
-    if (sector_after == sector_before):
-        return 0
-    else: 
-        return round((sector_after - sector_before) / float(ticks_after - ticks_before), 2)
+    assert sector_after != sector_before
+    assert ticks_after != ticks_before
+    return round((sector_after - sector_before) / float(ticks_after - ticks_before), 2)
 
-f = open("/sys/block/sdb/stat", "r")
-bs_sizes =    [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+device_opt = sys.argv[1]
+f = open("/sys/block/" + device_opt + "/stat", "r")
+bs_sizes =  [  1,   2,   4,   8,  16,  32,  64,  128, 256, 512, 1024, 2048, 4096]
+counts   =  [  5,   5,   5,   5,   3,   3,   3,    3,   3,   1,    1,    1,    1]    
 read_factors = []
 write_factors = []
 for i in range(0, len(bs_sizes)):
-    write_factors.append(get_factor(f, 2, bs_sizes[i]*1024))
-    read_factors.append(get_factor(f, 1, bs_sizes[i]*1024))
-print read_factors
-print write_factors
+    write_factors.append(get_factor(f, device_opt, 2, bs_sizes[i]*1024, counts[i]*1024))
+    read_factors.append(get_factor(f, device_opt, 1, bs_sizes[i]*1024, counts[i]*1024))
+print "READ-sector/ms:{0}".format(read_factors)
+print "WRITE-sector/ms:{0}".format(write_factors)
 f.close
